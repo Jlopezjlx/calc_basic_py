c63064d2e93f847a6b960312406793f157528c63
Jairo Lopez <jlopez@localhost.localdomain>
2019-04-05 21:02:58 -0400

    New

--- calc/calc.py
+++ calc/calc.py
@@ -16,19 +16,19 @@ while loop == True:
     try:
         if opcion == 1:
             resultado = maths.sumar(valor1,valor2)
-            print("La suma de %d + %d es %d" %(valor1, valor2, resultado))  
+            print("La suma de %d + %d es %d" % (valor1, valor2, resultado))  
         elif opcion == 2:
             resultado = maths.restar(valor1,valor2)
-            print("La resta de %d - %d es %d" %(valor1, valor2, resultado))
+            print("La resta de %d - %d es %d" % (valor1, valor2, resultado))
         elif opcion == 3:
             resultado = maths.multiplicacion(valor1,valor2)
-            print("La multiplicacion de %d * %d es %d" %(valor1, valor2, resultado))
+            print("La multiplicacion de %d * %d es %d" % (valor1, valor2, resultado))
         elif opcion == 4:
             resultado = maths.division(valor1,valor2)
-            print("La division de %d / %d es %d" %(valor1, valor2, float(resultado)))
+            print("La division de %d / %d es %d" % (valor1, valor2, float(resultado)))
         elif opcion == 5:
             resultado = maths.restante(valor1,valor2)
-            print("El restante de %d y %d es %d" %(valor1, valor2, resultado))
+            print("El restante de %d y %d es %d" % (valor1, valor2, resultado))
         else:
             print("Error en las entradas, Bye")
     except ZeroDivisionError:
